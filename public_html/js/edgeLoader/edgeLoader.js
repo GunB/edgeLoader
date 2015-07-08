e6bff246ae06332c6821c19ae71abc19e7757389
Heiner Angarita <heinerangarita@gmail.com>
2015-07-08 16:22:08 -0500

    Load

--- public_html/js/edgeLoader/edgeLoader.js
+++ public_html/js/edgeLoader/edgeLoader.js
@@ -4,3 +4,22 @@
  * and open the template in the editor.
  */
 
+debugging = false;
+
+window.EdgeLoadder ? true : window.EdgeLoadder = {
+    containers: {
+        "O_0": {
+            "type": "html", //De esto depende el tipo de carga que se realizará
+            "selector": "plantilla", //Identificador único de carga para ubicar los datos
+            "default_load": null //elemento en el arreglo de contenedores que será cargado por defecto
+        }
+    },
+    load_containers: function (precontainers) {
+        this.containers = $.extend({}, this.containers, precontainers);
+    },
+    data: {},
+    load_data: function (predata) {
+        
+    }
+
+};
